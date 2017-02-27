8b98eb3e8b4b136c36f9879f6700c45126fd1208
pfraed200 <peter_fraedrich@cable.comcast.com>
2017-02-27 14:52:31 -0500

    found issue with using commas in extravars, changed split char to semicolon

--- gofer.py
+++ gofer.py
@@ -22,8 +22,8 @@ parser = ArgumentParser(prog='gofer', description='Configurable intermediary pro
 parser.add_argument('--binary', '-b', required=True, help='The target binary name that will be run as a child process of this script')
 parser.add_argument('--systemd', '-s', required=False, help='the systemd unit name if different from the binary. If nothing is provided the name of the binary will be used.')
 parser.add_argument('--basedir', '-d', required=True, help='The base directory to look for the target binary')
-parser.add_argument('--extravars', '-e', required=False, help='@key=value,@key=value list of pairs of variables to pass into the command')
-parser.add_argument('--environment', '-v', required=False, help='@key=value,@key=value list of pairs of envuronment variables to defined in the thread')
+parser.add_argument('--extravars', '-e', required=False, help='@key=value;@key=value list of pairs of variables to pass into the command')
+parser.add_argument('--environment', '-v', required=False, help='@key=value;@key=value list of pairs of envuronment variables to defined in the thread')
 parser.add_argument('--command', '-c', required=True, nargs=REMAINDER, help='The command to run. The binary should be replaced with @binary to be templated out. Any other variables in --extravars should be represented by @varname and will be replaced at runtime. This should be the last argument given as it will capture everything after it.')
 args = parser.parse_args()
 
@@ -58,7 +58,7 @@ def split_extra_vars(extra_vars):
     if extra_vars == None:
         return {}
     v = {}
-    t = extra_vars.split(',')
+    t = extra_vars.split(';')
     for i in t:
         a = i.split('=')
         v[a[0]] = a[1]
@@ -89,7 +89,7 @@ def resolve_template(binary, cmd_list, env_vars, extra_vars):
     # add env vars to command
     if env_vars == None:
         return c
-    env = env_vars.split(',')
+    env = env_vars.split(';')
     e = ''
     for i in env:
         e += str(i) + ' '
